2c62e76a27dd0dae1e7fecbeaafab18b45bd3cce
Edmundo Alvarez <edmundo@graylog.com>
2016-06-01 15:11:53 +0200

    Display text if message is dropped in processing

--- src/web/simulator/SimulationPreview.jsx
+++ src/web/simulator/SimulationPreview.jsx
@@ -43,18 +43,31 @@ const SimulationPreview = React.createClass({
     }
 
     let simulationPreview = (this.props.isLoading ? <Spinner /> : null);
-    if (this.props.simulationResults) {
-      simulationPreview = this.props.simulationResults.map(message => {
-        return (
-          <MessageShow key={message.id}
-                       message={message}
-                       streams={streams}
-                       disableTestAgainstStream
-                       disableSurroundingSearch
-                       disableFieldActions
-                       isSimulation />
+    if (this.props.simulationResults && Array.isArray(this.props.simulationResults)) {
+      if (this.props.simulationResults.length === 0) {
+        simulationPreview = (
+          <Alert bsStyle="info">
+            <p><strong>Message would be dropped</strong></p>
+            <p>
+              Processing the loaded message would drop it from the system. That means that the message <strong>would
+              not be stored</strong>, and would not be available on searches, alerts, or dashboards.
+            </p>
+          </Alert>
         );
-      });
+      } else {
+        const messages = this.props.simulationResults.map(message => {
+          return (
+            <MessageShow key={message.id}
+                         message={message}
+                         streams={streams}
+                         disableTestAgainstStream
+                         disableSurroundingSearch
+                         disableFieldActions
+                         isSimulation />
+          );
+        });
+        simulationPreview = <div className="message-preview-wrapper">{messages}</div>;
+      }
     }
 
     let errorMessage;
@@ -65,7 +78,7 @@ const SimulationPreview = React.createClass({
           <p>
             Could not simulate processing of message <em>{this.props.originalMessage.id}</em> in stream{' '}
             <em>{this.props.stream.title}</em>.
-            <br/>
+            <br />
             Please try loading the message again, or use another message for the simulation.
           </p>
         </Alert>
@@ -87,10 +100,8 @@ const SimulationPreview = React.createClass({
         <Col md={6}>
           <h1>Simulation results</h1>
           <p>This is the result of processing the loaded message:</p>
-          <div className="message-preview-wrapper">
-            {errorMessage}
-            {simulationPreview}
-          </div>
+          {errorMessage}
+          {simulationPreview}
         </Col>
       </Row>
     );
