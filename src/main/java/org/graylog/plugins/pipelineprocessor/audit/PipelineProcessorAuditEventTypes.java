cf0c92e3fddf036fc935058c18f9f03804d99d63
Edmundo Alvarez <edmundo@graylog.com>
2016-08-23 13:00:47 +0200

    Integrate audit log

---
+++ src/main/java/org/graylog/plugins/pipelineprocessor/audit/PipelineProcessorAuditEventTypes.java
@@ -0,0 +1,33 @@
+package org.graylog.plugins.pipelineprocessor.audit;
+
+import com.google.common.collect.ImmutableSet;
+import org.graylog2.audit.PluginAuditEventTypes;
+
+import java.util.Set;
+
+public class PipelineProcessorAuditEventTypes implements PluginAuditEventTypes {
+    private static final String NAMESPACE = "pipeline_processor:";
+
+    public static final String PIPELINE_CONNECTION_EDIT = NAMESPACE + "pipeline_connection:edit";
+    public static final String PIPELINE_CREATE = NAMESPACE + "pipeline:create";
+    public static final String PIPELINE_UPDATE = NAMESPACE + "pipeline:update";
+    public static final String PIPELINE_DELETE = NAMESPACE + "pipeline:delete";
+    public static final String RULE_CREATE = NAMESPACE + "rule:create";
+    public static final String RULE_UPDATE = NAMESPACE + "rule:update";
+    public static final String RULE_DELETE = NAMESPACE + "rule:delete";
+
+    private static final Set<String> EVENT_TYPES = ImmutableSet.<String>builder()
+            .add(PIPELINE_CONNECTION_EDIT)
+            .add(PIPELINE_CREATE)
+            .add(PIPELINE_UPDATE)
+            .add(PIPELINE_DELETE)
+            .add(RULE_CREATE)
+            .add(RULE_UPDATE)
+            .add(RULE_DELETE)
+            .build();
+
+    @Override
+    public Set<String> auditEventTypes() {
+        return EVENT_TYPES;
+    }
+}
