baf46361f32c664996b9ba3874940a1c8fd00d9e
Kay Roepke <kroepke@googlemail.com>
2016-10-07 13:11:20 +0200

    remove old files left by improper rebase

--- plugin/src/web/pipeline-connections/PipelineConnectionsActions.js
+++
@@ -1,8 +0,0 @@
-import Reflux from 'reflux';
-
-const PipelineConnectionsActions = Reflux.createActions({
-  'list': {asyncResult: true},
-  'update': {asyncResult: true},
-});
-
-export default PipelineConnectionsActions;
