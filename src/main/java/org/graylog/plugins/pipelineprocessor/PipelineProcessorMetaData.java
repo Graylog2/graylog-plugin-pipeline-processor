5c9f014fd8f8ff01d32fbac199f0c2aea998ee00
Edmundo Alvarez <edmundo@graylog.com>
2016-11-04 15:46:28 +0100

    Bumping versions to 1.1.2/2.1.2

--- src/main/java/org/graylog/plugins/pipelineprocessor/PipelineProcessorMetaData.java
+++ src/main/java/org/graylog/plugins/pipelineprocessor/PipelineProcessorMetaData.java
@@ -50,7 +50,7 @@ public class PipelineProcessorMetaData implements PluginMetaData {
 
     @Override
     public Version getVersion() {
-        return new Version(1, 1, 1);
+        return new Version(1, 1, 2);
     }
 
     @Override
