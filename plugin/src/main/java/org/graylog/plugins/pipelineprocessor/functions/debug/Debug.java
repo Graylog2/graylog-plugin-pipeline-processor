169cf83d9045c91a5a84745fc4a627b618af14f5
Lennart Koopmann <lennart@graylog.com>
2017-05-29 17:20:08 +0200

    New function: debug()
    Added a new function that will log the string represntation of any value that has been passed to it. I just found this immensely helpful when working with a lookup table result.
    
    (cherry picked from commit fbe8cfb81e8c90f4b299c54cae8594642a57bec0)

---
+++ plugin/src/main/java/org/graylog/plugins/pipelineprocessor/functions/debug/Debug.java
@@ -0,0 +1,45 @@
+package org.graylog.plugins.pipelineprocessor.functions.debug;
+
+import org.graylog.plugins.pipelineprocessor.EvaluationContext;
+import org.graylog.plugins.pipelineprocessor.ast.functions.AbstractFunction;
+import org.graylog.plugins.pipelineprocessor.ast.functions.FunctionArgs;
+import org.graylog.plugins.pipelineprocessor.ast.functions.FunctionDescriptor;
+import org.graylog.plugins.pipelineprocessor.ast.functions.ParameterDescriptor;
+
+import static com.google.common.collect.ImmutableList.of;
+
+public class Debug extends AbstractFunction<Void> {
+
+    private final ParameterDescriptor<Object, Object> valueParam;
+
+    public static final String NAME = "debug";
+
+    Debug() {
+        valueParam = ParameterDescriptor.object("value").description("The value to print in the graylog-server log.").build();
+    }
+
+    @Override
+    public Void evaluate(FunctionArgs args, EvaluationContext context) {
+        final Object value = valueParam.required(args, context);
+
+        if(value == null) {
+            log.info("PIPELINE DEBUG: Passed value is NULL.");
+        } else {
+            log.info("PIPELINE DEBUG: {}", value.toString());
+        }
+
+        return null;
+    }
+
+    @Override
+    public FunctionDescriptor<Void> descriptor() {
+        return FunctionDescriptor.<Void>builder()
+                .name(NAME)
+                .returnType(Void.class)
+                .params(of(valueParam) )
+                .description("Print any passed value as string in the graylog-server log. Note that this will only appear in the " +
+                        "log of the graylog-server node that is processing the message you are trying to debug.")
+                .build();
+    }
+
+}
